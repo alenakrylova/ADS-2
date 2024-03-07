94899bbbf8582b839c448284c6decf6206397045
alena krylova <150517805+alenakrylova@users.noreply.github.com>
2024-03-07 14:41:08 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,45 @@
 
 
 double pown(double value, uint16_t n) {
-
+double res = 1.0;
+  for (uint64_t i = 0; i < n; i++) {
+    res *= value;
+  }
+  return res;
 }
 
 uint64_t fact(uint16_t n) {
-
+  uint64_t res = 1;
+  for (uint64_t i = 1; i <= n; i++) {
+    res *= i;
+  }
+  return res;
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return (pown(x, n) / fact(n));
 }
 
 double expn(double x, uint16_t count) {
-
+  double res = 1.0;
+  for (uint64_t i = 1; i <= count; i++) {
+    res += calcItem(x, i);
+  }
+  return res;
 }
 
 double sinn(double x, uint16_t count) {
-
+  double res = x;
+  for (uint64_t i = 2; i <= count; i++) {
+    res += calcItem(x, (2 * i) - 1) * pown((-1.0), i - 1);
+  }
+  return res;
 }
 
 double cosn(double x, uint16_t count) {
-
+  double res = 1;
+  for (uint64_t i = 2; i <= count; i++) {
+    res += calcItem(x, (2 * i) - 2) * pown((-1.0), i - 1);
+  }
+  return res;
 }
